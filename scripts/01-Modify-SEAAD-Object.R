5938e4a5491dd7bd7af09204c5b9f73434ffc872
Andrew Gjelsteen <akg@scc-wl4.scc.bu.edu>
2024-04-23 01:59:57 -0400

    first commit

---
+++ scripts/01-Modify-SEAAD-Object.R
@@ -0,0 +1,13 @@
+library(Seurat)
+library(data.table)
+library(dplyr)
+library(Matrix)
+
+# 1. Load the RDS Object (SEAAD DLPFC) ####
+seaad <- readRDS('/projectnb/tcwlab/LabMember/adpelle1/projects/singlecell-brain/outputs/01-SEAAD_data/SEAAD_DLPFC_43k.rds')
+
+out <- 'outputs/01-Modify-SEAAD-Object/'
+dir.create(out)
+
+saveRDS(seaad, file = paste0(out, 'SEAAD_DLPFC.rds'))
+
